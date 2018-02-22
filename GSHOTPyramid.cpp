2fde10fc68f86632d2ab18baffcc29a4b95c6c89
TFisichella <tfisiche@gmail.com>
2018-02-22 17:20:38 +0100

    gshotPyramid minimal

--- GSHOTPyramid.cpp
+++ GSHOTPyramid.cpp
@@ -1,5 +1,5 @@
 
-#include "GSHOTPyramid.hpp"
+#include "GSHOTPyramid.h"
 
 using namespace Eigen;
 using namespace FFLD;
@@ -7,129 +7,189 @@ using namespace std;
 
 //TODO Constructors for levels
 
-/** CONSTRUCTORS**/
-#pragma mark -  CONSTRUCTORS METHODS
+///** CONSTRUCTORS**/
+//#pragma mark -  CONSTRUCTORS METHODS
 
-GSHOTPyramid::GSHOTPyramid(): _octaves(0), _original_resolution(0),height_(0),levels_(0,0,0)
+
+GSHOTPyramid::GSHOTPyramid() : pad_( Eigen::Vector3i(0, 0, 0)), interval_(0)
 {
-    _descriptors = new std::vector<typename pcl::PointCloud<DescriptorType>::Ptr >();
-    _keypoints = new std::vector<typename pcl::PointCloud<PointType>::Ptr >();
-
 }
 
-GSHOTPyramid::GSHOTPyramid(const GSHOTPyramid &fp):levels_(0,0,0)
+GSHOTPyramid::GSHOTPyramid(const pcl::PointCloud<PointType>::Ptr input, Eigen::Vector3i pad, int interval) :
+    pad_( Eigen::Vector3i(0, 0, 0)), interval_(0)
 {
-    *this = fp;
-}
+    if (input.empty() || (pad.x < 1) || (pad.y < 1) || (pad.z < 1) || (interval < 1)) {
+        cerr << "Attempting to create an empty pyramid" << endl;
+        return;
+    }
 
-GSHOTPyramid::GSHOTPyramid(typename pcl::PointCloud<PointType>::Ptr input, int octaves, int interval, float starting_resolution, float starting_kp_grid_reso, float starting_descr_rad): _octaves(octaves), _original_resolution(starting_resolution),levels_(0,0,0)
-{
+    //TODO replace image per input
+    // Compute the number of scales such that the smallest size of the last level is 5
+    const int maxScale = ceil(log(min(image.width(), image.height()) / 40.0) / log(2.0) * interval);
+
+    // Cannot compute the pyramid on images too small
+    if (maxScale < interval) {
+        cerr << "The image is too small to create a pyramid" << endl;
+        return;
+    }
+
+    pad_ = pad;
     interval_ = interval;
-    
-    if(octaves < 0 || interval < 0 || starting_resolution < 0) {
-        std::cerr << "Arguments of the constructor cannot be negative!" << std::endl;
-        exit(0);
-    }
-    if(octaves ==0) {
-        std::cerr << "Not very useful to build a pyramid with no octaves..." << std::endl;
-        exit(0);
-    }
-    
-    _descriptors = new std::vector<typename pcl::PointCloud<DescriptorType>::Ptr >();
-    _keypoints = new std::vector<typename pcl::PointCloud<PointType>::Ptr >();
-    
-    PointType min;
-    PointType max;
-    
-    typename pcl::PointCloud<PointType>::Ptr normalized_input(new pcl::PointCloud<PointType>());
-    
-    //Define a resolution standard. Not advised to use full resolution.
-    if(starting_resolution>0) {
-        
-        pcl::UniformSampling<PointType> pre_sampling;
-        pre_sampling.setInputCloud (input);
-        pre_sampling.setRadiusSearch (starting_resolution);
-        pre_sampling.filter (*normalized_input);
-        
-        pcl::getMinMax3D(*normalized_input,min,max);
-        
-        _keypoints->push_back(this->compute_keypoints(normalized_input, starting_kp_grid_reso, min, max));
-        pcl::PointCloud<DescriptorType>::Ptr desc = this->compute_space(normalized_input,_keypoints->at(0),starting_descr_rad);
-        _descriptors->push_back(desc);
-    }
-    else {
-        typename pcl::PointCloud<PointType>::Ptr normalized_input = input;
-        
-        pcl::getMinMax3D(*normalized_input,min,max);
-        
-        _keypoints->push_back(this->compute_keypoints(normalized_input, starting_kp_grid_reso, min, max));
-        pcl::PointCloud<DescriptorType>::Ptr desc = this->compute_space(normalized_input,_keypoints->at(0),starting_descr_rad);
-        _descriptors->push_back(desc);
-    }
-    float resolution;
-    float kp_resolution;
-    float descr_rad;
-    
-    for(unsigned int i=1;i<=octaves;i++){
-        std::cout << "OCTAVE NUMBER " << i <<  std::endl;
-        resolution = starting_resolution*pow(2,i-1);
-        kp_resolution = starting_kp_grid_reso*i;
-        descr_rad = starting_descr_rad*i;
-        
-        for(unsigned int j=0;j<=interval;j++){
-            std::cout << "INTERVAL NUMBER " << j <<  std::endl;
-            typename pcl::PointCloud<PointType>::Ptr subspace(new typename pcl::PointCloud<PointType>());
-            float subresolution = resolution;
-            float sub_kp_resolution = kp_resolution;
-            float sub_descr_rad = descr_rad;
-            pcl::UniformSampling<PointType> sampling;
-            
-            subresolution += (j+1)*resolution/(interval+1);
-            sub_kp_resolution += (j+1)*kp_resolution/(interval+1);
-            sub_descr_rad += (j+1)*descr_rad/(interval+1);
-            
-            sampling.setInputCloud(normalized_input);
-            sampling.setRadiusSearch (subresolution);
-            sampling.filter(*subspace);
-            
-            _keypoints->push_back(compute_keypoints(normalized_input, sub_kp_resolution, min, max));
-            pcl::PointCloud<DescriptorType>::Ptr desc = compute_space(subspace,_keypoints->back(),sub_descr_rad);
-            _descriptors->push_back(desc);
+    levels_.resize(maxScale + 1);
+
+#pragma omp parallel for
+    for (int i = 0; i < interval; ++i) {
+        //TODO how to compute the grid_reso to use from interval ???
+
+        //Scale factor
+        //const double scale = pow(2.0, -static_cast<double>(i) / interval);
+        float grid_reso = 1.0;
+
+        PointCloudPtr keypoints = compute_keypoints(input, grid_reso, min, max);
+        PointCloudPtr descriptors = compute_descriptor(input, keypoints, descr_rad);
+
+        Cell* cell = *(levels_[i]);
+        for( int j = 0; j < descriptors.size(); ++j){
+
+            *cell = descriptors(j);
+            ++cell;
         }
     }
-    height_ = _descriptors->size();
-    
-}
-
-GSHOTPyramid::~GSHOTPyramid()
-{
-    _descriptors->clear();
-    _keypoints->clear();
-    height_ = 0;
 }
 
 
-void GSHOTPyramid::test() const
-{
+
+
+
+
+
+
+
+
+
+
+
+
+//GSHOTPyramid::GSHOTPyramid(): _octaves(0), _original_resolution(0),height_(0),levels_(0,0,0)
+//{
+//    _descriptors = new std::vector<typename pcl::PointCloud<DescriptorType>::Ptr >();
+//    _keypoints = new std::vector<typename pcl::PointCloud<PointType>::Ptr >();
+
+//}
+
+//GSHOTPyramid::GSHOTPyramid(const GSHOTPyramid &fp):levels_(0,0,0)
+//{
+//    *this = fp;
+//}
+
+//GSHOTPyramid::GSHOTPyramid(pcl::PointCloud<PointType>::Ptr input, int octaves, int interval, float starting_resolution, float starting_kp_grid_reso, float starting_descr_rad): _octaves(octaves), _original_resolution(starting_resolution),levels_(0,0,0)
+//{
+//    interval_ = interval;
     
-    //Tensor = pile de matrice
-    //Init array of float 32 x 1
-    ArrayXf a = ArrayXf::Random(32,1);
-    //Type,row,col
-    //Create a cell with the array created before, corresponding to descriptors
-    Eigen::Array<float, 32, 1> cell_test(a);
-    std::cout << cell_test.size() << std::endl;
-    //Rank 1 row = 2 col = 2
-    //Ligne colomne et depth
-    Tensor<Eigen::Array<float, 32, 1> > Level(2,2,2);
-    Level.setValues({{{cell_test, cell_test}, {cell_test, cell_test}},
-        {{cell_test, cell_test}, {cell_test, cell_test}}
-    });
+//    if(octaves < 0 || interval < 0 || starting_resolution < 0) {
+//        std::cerr << "Arguments of the constructor cannot be negative!" << std::endl;
+//        exit(0);
+//    }
+//    if(octaves ==0) {
+//        std::cerr << "Not very useful to build a pyramid with no octaves..." << std::endl;
+//        exit(0);
+//    }
     
-    // Result is a zero dimension tensor
+//    _descriptors = new std::vector<typename pcl::PointCloud<DescriptorType>::Ptr >();
+//    _keypoints = new std::vector<typename pcl::PointCloud<PointType>::Ptr >();
     
-    //m_Level
-}
+//    PointType min;
+//    PointType max;
+    
+//    typename pcl::PointCloud<PointType>::Ptr normalized_input(new pcl::PointCloud<PointType>());
+    
+//    //Define a resolution standard. Not advised to use full resolution.
+//    if(starting_resolution>0) {
+        
+//        pcl::UniformSampling<PointType> pre_sampling;
+//        pre_sampling.setInputCloud (input);
+//        pre_sampling.setRadiusSearch (starting_resolution);
+//        pre_sampling.filter (*normalized_input);
+        
+//        pcl::getMinMax3D(*normalized_input,min,max);
+        
+//        _keypoints->push_back(this->compute_keypoints(normalized_input, starting_kp_grid_reso, min, max));
+//        pcl::PointCloud<DescriptorType>::Ptr desc = this->compute_descriptor(normalized_input,_keypoints->at(0),starting_descr_rad);
+//        _descriptors->push_back(desc);
+//    }
+//    else {
+//        typename pcl::PointCloud<PointType>::Ptr normalized_input = input;
+        
+//        pcl::getMinMax3D(*normalized_input,min,max);
+        
+//        _keypoints->push_back(this->compute_keypoints(normalized_input, starting_kp_grid_reso, min, max));
+//        pcl::PointCloud<DescriptorType>::Ptr desc = this->compute_descriptor(normalized_input,_keypoints->at(0),starting_descr_rad);
+//        _descriptors->push_back(desc);
+//    }
+//    float resolution;
+//    float kp_resolution;
+//    float descr_rad;
+    
+//    for(unsigned int i=1;i<=octaves;i++){
+//        std::cout << "OCTAVE NUMBER " << i <<  std::endl;
+//        resolution = starting_resolution*pow(2,i-1);
+//        kp_resolution = starting_kp_grid_reso*i;
+//        descr_rad = starting_descr_rad*i;
+        
+//        for(unsigned int j=0;j<=interval;j++){
+//            std::cout << "INTERVAL NUMBER " << j <<  std::endl;
+//            typename pcl::PointCloud<PointType>::Ptr subspace(new typename pcl::PointCloud<PointType>());
+//            float subresolution = resolution;
+//            float sub_kp_resolution = kp_resolution;
+//            float sub_descr_rad = descr_rad;
+//            pcl::UniformSampling<PointType> sampling;
+            
+//            subresolution += (j+1)*resolution/(interval+1);
+//            sub_kp_resolution += (j+1)*kp_resolution/(interval+1);
+//            sub_descr_rad += (j+1)*descr_rad/(interval+1);
+            
+//            sampling.setInputCloud(normalized_input);
+//            sampling.setRadiusSearch (subresolution);
+//            sampling.filter(*subspace);
+            
+//            _keypoints->push_back(compute_keypoints(normalized_input, sub_kp_resolution, min, max));
+//            pcl::PointCloud<DescriptorType>::Ptr desc = compute_descriptor(subspace,_keypoints->back(),sub_descr_rad);
+//            _descriptors->push_back(desc);
+//        }
+//    }
+//    height_ = _descriptors->size();
+    
+//}
+
+//GSHOTPyramid::~GSHOTPyramid()
+//{
+//    _descriptors->clear();
+//    _keypoints->clear();
+//    height_ = 0;
+//}
+
+
+//void GSHOTPyramid::test() const
+//{
+    
+//    //Tensor = pile de matrice
+//    //Init array of float 32 x 1
+//    ArrayXf a = ArrayXf::Random(32,1);
+//    //Type,row,col
+//    //Create a cell with the array created before, corresponding to descriptors
+//    Eigen::Array<float, 32, 1> cell_test(a);
+//    std::cout << cell_test.size() << std::endl;
+//    //Rank 1 row = 2 col = 2
+//    //Ligne colomne et depth
+//    Tensor<Eigen::Array<float, 32, 1> > Level(2,2,2);
+//    Level.setValues({{{cell_test, cell_test}, {cell_test, cell_test}},
+//        {{cell_test, cell_test}, {cell_test, cell_test}}
+//    });
+    
+//    // Result is a zero dimension tensor
+    
+//    //m_Level
+//}
 
 
 
@@ -138,10 +198,11 @@ void GSHOTPyramid::test() const
 /*
  * WARNING : need to build a sub structure to partially specialize the pyramid
  */
-typename pcl::PointCloud<DescriptorType>::Ptr GSHOTPyramid::compute_space(typename pcl::PointCloud<PointType>::Ptr input, typename pcl::PointCloud<PointType>::Ptr keypoints, float descr_rad)
+PointCloudPtr
+GSHOTPyramid::compute_descriptor(PointCloudPtr input, PointCloudPtr keypoints, float descr_rad)
 {
-    pcl::PointCloud<DescriptorType>::Ptr descriptors (new pcl::PointCloud<DescriptorType> ());
-    pcl::PointCloud<NormalType>::Ptr normals (new pcl::PointCloud<NormalType> ());
+    PointCloudPtr descriptors (new pcl::PointCloud<DescriptorType> ());
+    PointCloudPtr normals (new pcl::PointCloud<NormalType> ());
 
     //pcl::SHOTEstimation<PointType, NormalType, DescriptorType> norm_est;
     pcl::NormalEstimation<PointType,NormalType> norm_est;
@@ -159,7 +220,8 @@ typename pcl::PointCloud<DescriptorType>::Ptr GSHOTPyramid::compute_space(typena
     return descriptors;
 }
 
-typename pcl::PointCloud<PointType>::Ptr GSHOTPyramid::compute_keypoints(typename pcl::PointCloud<PointType>::Ptr input, float grid_reso, PointType min, PointType max) {
+pcl::PointCloud<PointType>::Ptr
+GSHOTPyramid::compute_keypoints(pcl::PointCloud<PointType>::Ptr input, float grid_reso, PointType min, PointType max){
     
     int pt_nb_x = (int)((max.x-min.x)/grid_reso+1);
     int pt_nb_y = (int)((max.y-min.y)/grid_reso+1);
@@ -192,97 +254,102 @@ typename pcl::PointCloud<PointType>::Ptr GSHOTPyramid::compute_keypoints(typenam
     return keypoints;
 }
 
-/** GETTER AND SETTERS **/
-#pragma mark -  GETTERS AND SETTER METHODS
+///** GETTER AND SETTERS **/
+//#pragma mark -  GETTERS AND SETTER METHODS
 
-pcl::PointCloud<DescriptorType> GSHOTPyramid::get_descriptors_layer(unsigned int i)
-{
-    return *(_descriptors->at(i));
-}
+//pcl::PointCloud<DescriptorType> GSHOTPyramid::get_descriptors_layer(unsigned int i)
+//{
+//    return *(_descriptors->at(i));
+//}
 
-pcl::PointCloud<PointType> GSHOTPyramid::get_keypoints_layer(unsigned int i)
-{
-    return *(_keypoints->at(i));
-}
+//pcl::PointCloud<PointType> GSHOTPyramid::get_keypoints_layer(unsigned int i)
+//{
+//    return *(_keypoints->at(i));
+//}
 
-const GSHOTPyramid::Tensor<GSHOTPyramid::Cell> GSHOTPyramid::getLevels() const{
-    
-    return levels_;
-}
-
-int GSHOTPyramid::get_octaves()
-{
-    return _octaves;
-}
-int GSHOTPyramid::get_sub_between_octaves()
-{
-    return (_descriptors->size()-1)/(_octaves+1);
-}
-
-int GSHOTPyramid::get_height()
-{
-    return height_;
-}
-
-float GSHOTPyramid::get_original_resolution()
-{
-    return _original_resolution;
-}
-
-Eigen::Vector3i GSHOTPyramid::getLayerTopology(int i){
-    
-    return topology[i];
-}
-
-float GSHOTPyramid::get_layer_resolution(int i)
-{
-    int oct = (int) (i/get_octaves());
-    int pos_in_octave = i%get_octaves();
-    float dist_btw_octave = _original_resolution/pow(2,oct) - _original_resolution/pow(2,oct+1);
-    return _original_resolution/pow(2,oct) - dist_btw_octave*pos_in_octave/(get_sub_between_octaves()+1);
-}
-/*
- * Need to declare every descriptor we intend to use
- */
-const char* GSHOTPyramid::get_descriptor_type()
-{
-    //return typeid(DescriptorType).name();
-    return "SHOT352";
-}
-
-const char* GSHOTPyramid::get_point_type()
-{
-    //return typeid(PointType).name();
-    return "XRZRGB";
-}
-
-int GSHOTPyramid::getNumberInterval() const
+int GSHOTPyramid::interval() const
 {
     return interval_;
 }
 
+Eigen::Vector3i HOGPyramid::pad() const
+{
+    return pad_;
+}
+
 bool GSHOTPyramid::isEmpty() const
 {
-    //return levels().empty();
+    return levels_.empty();
 }
 
-
-
-void GSHOTPyramid::toString()
-{
+const vector<GSHOTPyramid::Level> & GSHOTPyramid::levels() const{
     
-    std::cout<<"Pyramid specifications :"<<std::endl<<std::endl;
-    std::cout<<"Descriptor type : "<<this->get_descriptor_type()<<std::endl;
-    std::cout<<"Point type : "<<this->get_point_type()<<std::endl;
-    std::cout<<"Number of octaves : "<<this->_octaves<<std::endl;
-    std::cout<<"Height : "<<this->_descriptors->size()<<" layers ("<<get_sub_between_octaves()<<" subsections between octaves)"<<std::endl;
-    if(_original_resolution==0) {
-        std::cout<<"No presampling used, pyramid starting at the original resolution."<<std::endl;
-    }
-    else {
-        std::cout << "Resolution of the first floor : "<<_original_resolution<<std::endl;
-    }
+    return levels_;
 }
+
+//int GSHOTPyramid::get_octaves()
+//{
+//    return _octaves;
+//}
+//int GSHOTPyramid::get_sub_between_octaves()
+//{
+//    return (_descriptors->size()-1)/(_octaves+1);
+//}
+
+//int GSHOTPyramid::get_height()
+//{
+//    return height_;
+//}
+
+//float GSHOTPyramid::get_original_resolution()
+//{
+//    return _original_resolution;
+//}
+
+//Eigen::Vector3i GSHOTPyramid::getLayerTopology(int i){
+    
+//    return topology[i];
+//}
+
+//float GSHOTPyramid::get_layer_resolution(int i)
+//{
+//    int oct = (int) (i/get_octaves());
+//    int pos_in_octave = i%get_octaves();
+//    float dist_btw_octave = _original_resolution/pow(2,oct) - _original_resolution/pow(2,oct+1);
+//    return _original_resolution/pow(2,oct) - dist_btw_octave*pos_in_octave/(get_sub_between_octaves()+1);
+//}
+///*
+// * Need to declare every descriptor we intend to use
+// */
+//const char* GSHOTPyramid::get_descriptor_type()
+//{
+//    //return typeid(DescriptorType).name();
+//    return "SHOT352";
+//}
+
+//const char* GSHOTPyramid::get_point_type()
+//{
+//    //return typeid(PointType).name();
+//    return "XRZRGB";
+//}
+
+
+
+//void GSHOTPyramid::toString()
+//{
+    
+//    std::cout<<"Pyramid specifications :"<<std::endl<<std::endl;
+//    std::cout<<"Descriptor type : "<<this->get_descriptor_type()<<std::endl;
+//    std::cout<<"Point type : "<<this->get_point_type()<<std::endl;
+//    std::cout<<"Number of octaves : "<<this->_octaves<<std::endl;
+//    std::cout<<"Height : "<<this->_descriptors->size()<<" layers ("<<get_sub_between_octaves()<<" subsections between octaves)"<<std::endl;
+//    if(_original_resolution==0) {
+//        std::cout<<"No presampling used, pyramid starting at the original resolution."<<std::endl;
+//    }
+//    else {
+//        std::cout << "Resolution of the first floor : "<<_original_resolution<<std::endl;
+//    }
+//}
 
 
 
--- GSHOTPyramid.h
+++ GSHOTPyramid.h
@@ -1,8 +1,5 @@
 #ifndef FFLD_GSHOTPYRAMID_H
 #define FFLD_GSHOTPYRAMID_H
-//TODO CHANGE PADX PADY PADZ to (
-
-#include "JPEGImage.h"
 
 //EIGEN
 #include <Eigen/Core>
@@ -52,7 +49,8 @@ namespace FFLD
             Tensor( int s1, int s2, int s3) : Eigen::Tensor<Type, 3, Eigen::RowMajor>(s1, s2, s3){}
             //row d'une matrice --> renvoie ligne de la matrice
             
-            Eigen::Matrix<Type, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> row( int i) const{
+            Eigen::Matrix<Type, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>
+            row( int i) const{
                 Eigen::Matrix<Type, this->dimension(0), this->dimension(1), Eigen::RowMajor> res;
                 return res.setZero();
             }
@@ -76,15 +74,23 @@ namespace FFLD
         // Copy constructor
         GSHOTPyramid(const GSHOTPyramid&);
         
+        /// Constructs a pyramid from the JPEGImage of a Scene.
+        /// @param[in] input The PointCloud data
+        /// @param[in] pad Amount of horizontal, vertical and depth zero padding (in cells).
+        /// @param[in] interval Number of levels per octave in the pyramid.
+        /// @note The amount of padding and the interval should be at least 1.
+        GSHOTPyramid(const pcl::PointCloud<PointType>::Ptr input, Eigen::Vector3i pad, int interval = 5);
 
         /// Constructs a pyramid from a given point cloud data.
         /// @param[in] input The PointCloud data
         /// @param[in] octave Amount of Octaves in the pyramid (An octave can have muliples subsections/intervals)
         /// @param[in] interval Number of intervals in each octave/Nuber of levels per octave in the pyramid
-        /// @param[in] starting_resolution Starting resolution for the point cloud. So that we can pre sample the point cloud, in order to reduce computation and define a standard.
+        /// @param[in] starting_resolution Starting resolution for the point cloud. So that we can pre sample the point cloud,
+        /// in order to reduce computation and define a standard.
         /// @param[in] starting_kp_grid_reso Resolution keypoint
         /// @note TODO Need to take a container of parameters instead of the descr_rad to generalize to different descriptors.
-        GSHOTPyramid(typename pcl::PointCloud<PointType>::Ptr input,  int octaves, int interval, float starting_resolution, float starting_kp_grid_reso, float descr_rad);
+        GSHOTPyramid(pcl::PointCloud<PointType>::Ptr input,  int octaves, int interval,
+                     float starting_resolution, float starting_kp_grid_reso, float descr_rad);
         
         // Destructor
         ~GSHOTPyramid();
@@ -99,7 +105,7 @@ namespace FFLD
         
         /// Returns the pyramid levels.
         /// @note Scales are given by the following formula: 2^(1 - @c index / @c interval).
-        const Tensor<Cell> getLevels() const;
+        const std::vector<Level> & levels() const;
         //Get keypoints at a given level
         Eigen::Vector3i getLayerTopology(int i);
         pcl::PointCloud<DescriptorType> get_descriptors_layer(unsigned int);
@@ -123,7 +129,7 @@ namespace FFLD
         /// Returns the convolutions of the pyramid with a filter.
         /// @param[in] filter Filter.
         /// @param[out] convolutions Convolution of each level.
-        void convolve(const Level & filter, std::vector<Matrix> & convolutions) const;
+        void convolve(const Level & filter, std::vector<Tensor> & convolutions) const;
         
         /// Returns the flipped version (horizontally) of a level.
         static GSHOTPyramid::Level Flip(const GSHOTPyramid::Level & level);
@@ -142,28 +148,37 @@ namespace FFLD
         private:
         
         // Method for computing feature spaces. Will have different implementations depending on the descriptor used.
-        typename pcl::PointCloud<DescriptorType>::Ptr compute_space(typename pcl::PointCloud<PointType>::Ptr input, typename pcl::PointCloud<PointType>::Ptr keypoints, float);
+        PointCloudPtr
+        compute_descriptor(PointCloudPtr input, PointCloudPtr keypoints, float);
         
         // Method creating the keypoint grid using the min/max values of the input
-        typename pcl::PointCloud<PointType>::Ptr compute_keypoints(typename pcl::PointCloud<PointType>::Ptr input, float grid_reso, PointType min, PointType max);
+        PointCloudPtr
+        compute_keypoints(PointCloudPtr input, float grid_reso, PointType min, PointType max);
         
-        // Container of the different descriptor layers from 0 (original resolution) to n (lowest resolution, last octave)
-        std::vector<typename pcl::PointCloud<DescriptorType>::Ptr >* _descriptors;
+//        // Container of the different descriptor layers from 0 (original resolution) to n (lowest resolution, last octave)
+//        std::vector<typename pcl::PointCloud<DescriptorType>::Ptr >* _descriptors;
         
-        // Container of the different keypoint layers from 0 (original resolution) to n (lowest resolution, last octave)
-        std::vector<typename pcl::PointCloud<PointType>::Ptr >* _keypoints;
+
         
 
         // Computes the 2D convolution of a pyramid level with a filter
         static void Convolve(const Level & x, const Level & y, Matrix & z);
         
-        // Number of keypoints per dimension (needed for the sliding box process)
-        std::vector<Eigen::Vector3i,Eigen::aligned_allocator<Eigen::Vector3i> > topology;
-        int _octaves;
-        float _original_resolution;
+//        // Number of keypoints per dimension (needed for the sliding box process)
+//        std::vector<Eigen::Vector3i,Eigen::aligned_allocator<Eigen::Vector3i> > topology;
+//        int _octaves;
+//        float _original_resolution;
+//        int interval_;
+//        int height_;
+//        Tensor<Cell> levels_;
+
+        Eigen::Vector3i pad_;
         int interval_;
-        int height_;
-        Tensor<Cell> levels_;
+        // Represent a vector of 3D scene of descriptors computed at different resolution
+        //from 0 (original resolution) to n (lowest resolution, last octave)
+        std::vector<Level> levels_;
+        // The corresponding positions of the descriptors in the space for each level
+        std::vector<PointCloudPtr > _keypoints;
     };
     
     /// Serializes a pyramid to a stream.
